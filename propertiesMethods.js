62b0e5e7231778f5ca552ecb691bc9536a1a79c4
amanicps <80164449+AmaniMango@users.noreply.github.com>
2024-04-08 14:32:21 +0000

    done w assignment

--- propertiesMethods.js
+++ propertiesMethods.js
@@ -1,23 +1,25 @@
 let myArray1 = [1, 2, 3, 4, 5];
 
 // 1. Console log the length of myArray1
-console.log(); // Should print 5
+console.log(myArray1); // Should print 5
 
-
+console.log("length" + myArray1.length);
 
 
 let myArray2 = [1, 2, 3];
+myArray2.push(4);
 // 2. Add the number 4 to the end of myArray2
 
 
 console.log(myArray2); // Should print [1, 2, 3, 4]
+console.log("length" + myArray2.length);
 
 
 
 
 let myArray3 = [1, 2, 3, 4];
 // 3. Remove the last element from myArray3
-
+myArray3.pop(4);
 
 console.log(myArray3); // Should print [1, 2, 3]
 
@@ -26,16 +28,14 @@ console.log(myArray3); // Should print [1, 2, 3]
 
 let myArray4 = [1, 2, 3, 4];
 // 4. Remove the first element from myArray4
-
-
+myArray4.shift();
 console.log(myArray4); // Should print [2, 3, 4]
 
 
 
 let myArray5 = [2, 3, 4];
 // 5. Add the number 1 to the beginning of myArray5
-
-
+myArray1.unshift("1");
 console.log(myArray5); // Should print [1, 2, 3, 4]
 
 
